92c14812997be7b3345f9b5f2e19f4999cb00d46
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-10-20 22:45:19 -0600

    first commit

---
+++ src/components/static/footer.tsx
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Footer of all pages
+ * 
+ */
+
+function FooterComponent(){
+
+    return (
+        <>
+
+            <footer>
+                    <h2>Footer</h2>
+            </footer>
+
+        </>
+    )
+
+}
+
+
+export default FooterComponent;
---
+++ src/components/static/header.tsx
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Header of all pages
+ * 
+ */
+
+function HeaderComponent(){
+
+    return (
+        <>
+
+            <header>
+                <h2>Header</h2>
+            </header>
+
+        </>
+    )
+
+}
+
+
+export default HeaderComponent;
