0cf913f7719ee6f1f9cff36392632f05dc0f9a01
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-10-23 16:21:18 -0600

    Se agrego el seamos vos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AboutComponent from "@/components/pages/home/about";
 import ApiculturaComponent from "@/components/pages/home/apicultura";
 import BannerComponent from "@/components/pages/home/banner";
 import ConstribuirComponent from "@/components/pages/home/constribuir";
+import EntrevistasComponent from "@/components/pages/home/entrevistas";
 import MapaComponent from "@/components/pages/home/mapa";
 import NoticiasComponent from "@/components/pages/home/noticias";
 import PreguntasFrecuentesComponent from "@/components/pages/home/preguntas_frecuentes";
@@ -21,6 +22,7 @@ function Home(){
         <MapaComponent/>
         <NoticiasComponent/>
         <PreguntasFrecuentesComponent/>
+        <EntrevistasComponent/>
       </section>
     </>
   );
---
+++ src/components/pages/home/entrevistas.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import BorbujaImagen from '@/assets/img/borbuja_dos.png';
+import MariposasColorsImage from '@/assets/img/mariposa_colors.svg';
+
+function EntrevistasComponent(){
+
+
+    const entrevistas = [
+        {
+            title: "Entrevistado #12",
+            content: "Lo que debe suceder es lo que ha sucedido en productos como el café, darle la misma importancia, así como era bueno para exportación que también el producto sea consumido en el país"
+        },
+        {
+            title: "Entrevistado #22",
+            content: "Hay que fomentar la reproducción de las abejas, porque sabemos que el día que desaparezcan, el mundo entero desaparecería"
+        },
+        {
+            title: "Entrevistado #28",
+            content: "Hay que fomentar la reproducción de las abejas, porque sabemos que el día que desaparezcan, el mundo entero desaparecería"
+        },
+    ]
+
+    
+    return (
+        <>
+            <section className="entrevistas_component">
+                <section className="principal">
+                    <section className="borbula">
+                        <Image src={BorbujaImagen.src} alt="Imagen de personas con abejas" width={100} height={100} />
+                    </section>
+                </section>
+                <h2>Seamos voz <br/> <span>para las abejas</span></h2>
+                <section className="entrevistas">
+                    {
+                        entrevistas.map( (item, key) => {
+                            return (
+                                <section className="entrevista" key={key}>
+                                    <p>&quot;{item.content}&quot;</p>
+                                    <span>{ item.title }</span>
+                                    <section className="abeja_item">
+                                        <Image src={MariposasColorsImage} alt="Mariposas de colores" width={150} height={150} />
+                                    </section>
+                                </section>
+                            )
+                        })
+                    }
+                </section>
+            </section>
+        </>
+    )
+
+}
+
+export default EntrevistasComponent;
